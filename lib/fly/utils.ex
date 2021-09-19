3ec1e4cbbb5722e097ebd5c82e4344093d2f4bbb
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2021-09-18 22:04:53 -0300

    Handling of non-string arguments during timestamps parsing

--- lib/fly/utils.ex
+++ lib/fly/utils.ex
@@ -32,17 +32,44 @@ defmodule Fly.Utils do
   @doc """
   Converts a ISO 8601 date string to a human readable format.
   """
-  def readable_date(iso_8601_string) do
-    DateTime.from_iso8601(iso_8601_string)
+  def get_readable_date(iso8601_string)
+    when is_binary(iso8601_string) or is_bitstring(iso8601_string)
+    do
+
+    DateTime.from_iso8601(iso8601_string)
       |> case do
-      {:ok, datetime, 0}  ->
-        "#{DateTime.to_date(datetime)}, #{Time.truncate(DateTime.to_time(datetime), :second)}"
+        {:ok, datetime, 0}  ->
+            output_readable_date(datetime)
 
-      {:error, :invalid_format} ->
+        {:error, :invalid_format} ->
+          "Not available"
+
+        other ->
+          Logger.error("Unexpected result parsing ISO 9601 DateTime string. Result: #{inspect(other)}")
+
+          "Not available"
+      end
+  end
+
+  def get_readable_date(_iso8601_string) do
+    "Not available"
+  end
+
+  @doc """
+  Converts a DateTime struct to a human readable format.
+  """
+  def output_readable_date(datetime) do
+    try do
+      date = DateTime.to_date(datetime)
+      time = DateTime.to_time(datetime)
+      output_string = "#{date}, #{Time.truncate(time, :second)}"
+
+      output_string
+    rescue
+      FunctionClauseError ->
+        Logger.error("Unexpected result while generating string from ISO 9601 DateTime. Used DateTime: #{datetime}")
+
         "Not available"
-      other ->
-
-        Logger.error("Unexpected result parsing ISO 9601 DateTime string. Result: #{inspect(other)}")
     end
   end
 
